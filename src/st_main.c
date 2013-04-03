8cdf2ec8e2aa197b25429c9e5e087a4b107d5145
Marc Villacorta <marc.villacorta@gmail.com>
2013-04-03 23:10:33 +0200

    Code skeleton.

---
+++ src/st_main.c
@@ -0,0 +1,42 @@
+/******************************************************************************
+* Copyright (C) 2013 Marc Villacorta Morera
+*
+* Authors: Marc Villacorta Morera <marc.villacorta@gmail.com>
+*
+* This file is part of SlideTail.
+*
+* SlideTail is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* SlideTail is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with SlideTail. If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
+//-----------------------------------------------------------------------------
+// Includes:
+//-----------------------------------------------------------------------------
+
+#include "st_main.h"
+
+//-----------------------------------------------------------------------------
+// Globals:
+//-----------------------------------------------------------------------------
+
+
+//-----------------------------------------------------------------------------
+// Entry point:
+//-----------------------------------------------------------------------------
+
+int main(int argc, char *argv[])
+
+{
+    printf("Hello World!\n");
+    return 0;
+}
---
+++ src/st_main.h
@@ -0,0 +1,50 @@
+/******************************************************************************
+* Copyright (C) 2013 Marc Villacorta Morera
+*
+* Authors: Marc Villacorta Morera <marc.villacorta@gmail.com>
+*
+* This file is part of SlideTail.
+*
+* SlideTail is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* SlideTail is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with SlideTail. If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************/
+
+//-----------------------------------------------------------------------------
+// Include guard:
+//-----------------------------------------------------------------------------
+
+#ifndef _ST_
+#define _ST_
+
+//-----------------------------------------------------------------------------
+// Includes:
+//-----------------------------------------------------------------------------
+
+#define _GNU_SOURCE
+#include <stdio.h>
+
+//-----------------------------------------------------------------------------
+// Defines:
+//-----------------------------------------------------------------------------
+
+#define MyDBG(x) do {printf("(%d) %s:%d\n", errno, __FILE__, __LINE__); goto x;} while (0)
+
+//-----------------------------------------------------------------------------
+// Typedefs:
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+// End of include guard:
+//-----------------------------------------------------------------------------
+
+#endif
